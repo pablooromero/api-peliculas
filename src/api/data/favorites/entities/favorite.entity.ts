fc7252ef26487d7e0575a687aca820945528856e
pablooromero <pabloromerook@gmail.com>
2024-10-21 11:07:34 -0300

    fixes in tests & added documentation

--- src/api/data/favorites/entities/favorite.entity.ts
+++ src/api/data/favorites/entities/favorite.entity.ts
@@ -1,15 +1,19 @@
 import { User } from 'src/api/auth/entities/user.entity';
 import { Entity, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 import { Film } from '../../films/entities/film.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('favorites')
 export class Favorite {
+  @ApiProperty({example: 1})
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({example: 1})
   @ManyToOne(() => User, user => user.favorites)
   user: User;
 
+  @ApiProperty({example: 1})
   @ManyToOne(() => Film, film => film.favorites)
   film: Film;
 }
